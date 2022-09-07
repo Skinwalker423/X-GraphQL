248bb5284cb1c149f71d0701a19e5078f2f1f08c
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-06 17:31:51 -0700

    GraphQL with X-fashion

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+
+});
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items{
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
+export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS);
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+
+  console.log(loading);
+  console.log('data', data);
+
+  useEffect(() => {
+    console.log(data)
+  }, [data]);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
