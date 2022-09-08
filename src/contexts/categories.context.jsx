f10332ce3c42f2fe4836eba519e3b28651937859
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-07 21:02:10 -0700

    graphql

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,39 +1,22 @@
 import { createContext, useState, useEffect } from 'react';
-import { gql, useQuery } from '@apollo/client';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
-
 });
 
-const COLLECTIONS = gql`
-  query {
-    collections {
-      id
-      title
-      items{
-        id
-        name
-        price
-        imageUrl
-      }
-    }
-  }
-`
-
 export const CategoriesProvider = ({ children }) => {
-  const {loading, error, data} = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-
-  console.log(loading);
-  console.log('data', data);
-
   useEffect(() => {
-    console.log(data)
-  }, [data]);
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
 
   const value = { categoriesMap };
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -10,26 +9,19 @@ import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
-const client = new ApolloClient({
-  url: "https://crwn-clothing.com/",
-  cache: new InMemoryCache(),
-})
-
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
